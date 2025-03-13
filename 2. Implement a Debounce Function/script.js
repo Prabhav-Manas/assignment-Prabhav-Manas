22edfb222f066af9b26e1cccc5647dd6185e28cb
Prabhav Manas <prabhavmanas17@gmail.com>
2025-03-13 11:08:29 +0530

    updated-commit

--- 2. Implement a Debounce Function/script.js
+++ 2. Implement a Debounce Function/script.js
@@ -1,19 +1,22 @@
 const myDebounce=(callback, delay)=>{
   let timer;
 
-  return function(...args){
-    if(timer) clearTimeout(timer);
+  return function(...args){ //Returns a new function that takes an argument
+    if(timer) clearTimeout(timer); //Clear out the previous timer if a new call is made
 
     timer=setTimeout(()=>{
-      callback()
+      callback(...args) //Execute the callback with the provided arguments after the delay
     }, delay)
   }
 }
 
-const myFunc=()=>{
-  console.log('Function execution with debouncing!');
+// Function to check debouncing
+const myFunc=(name)=>{
+  console.log(`Hello, ${name}!`);
 }
 
+// Debounce function with 1-second delay
 const debounceFunc=myDebounce(myFunc, 1000);
 
-debounceFunc();
+// Call the debounced function
+debounceFunc('Prabhav'); //Will log "Hello, Prabhav" after 1-second delay
