b81b8cb8f197bb055702a8e3d3af7592c1e910b1
Prabhav Manas <prabhavmanas17@gmail.com>
2025-03-13 11:30:09 +0530

    'updated'

--- 4. Modify User List in HTML/script.js
+++ 4. Modify User List in HTML/script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-  const userList=document.querySelector("#user-list ul");
+  const userList=document.querySelector("#user-list ul"); //Select the unordered list with id user-list
 
   // ===Add New User===
-  const newUser=document.createElement('li');
-  newUser.textContent='David';
-  userList.appendChild(newUser);
+  const newUser=document.createElement('li'); //Create new list
+  newUser.textContent='David'; //Set the text content as David
+  userList.appendChild(newUser); //Append the new user list
 
   // ===Remove First User===
-  const firstUser=userList.querySelector('li');
-  if(firstUser) userList.removeChild(firstUser)
+  const firstUser=userList.querySelector('li'); //Select the first user from the list
+  if(firstUser) userList.removeChild(firstUser) //If the first user exists remove it
 })
